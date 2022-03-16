4f11da2d50b7bc454998da1920297536364a896f
joerodgers <28455042+joerodgers@users.noreply.github.com>
2022-03-16 09:48:56 -0400

    Update Deploy-O365Resources.ps1

--- Deploy-O365Resources.ps1
+++ Deploy-O365Resources.ps1
@@ -9,7 +9,7 @@ $tenantId   = $env:O365_TENANTID
 $tenant     = $env:O365_TENANT
 
 # you will see this as output in the Deploy-AzureResources.ps1 script or pull directly from the Logic App's trigger action.
-$powerAutomateOrLogicAppTriggerUrl = "https://prod-50.eastus.logic.azure.com:443/xxxxx"
+$powerAutomateOrLogicAppTriggerUrl = "https://prod-50.eastus.logic.azure.com:443/workflows/116677db9ed04621b15a49e04b3c6da9/triggers/manual/paths/invoke?api-version=2019-05-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=GImmL7u6oApxOC0XsMa_A_bwHzbfDyUMxEITxMDpxj8"
 
 # manually upload these files and update the paths below
 $previewImageUrl  = "https://$tenant.sharepoint.com/SiteAssets/contoso-logo.jpeg"
@@ -33,7 +33,7 @@ $previewImageUrl  = "https://$tenant.sharepoint.com/SiteAssets/contoso-logo.jpeg
         {{
         "verb" : "triggerFlow",
         "url"  : "{0}",
-        "name" : "Apply Site Template",
+        "name" : "Send Welcome Email",
         "parameters" : {{
             "event"    : "Site Creation",
             "product"  : "SharePoint Online"
@@ -43,39 +43,44 @@ $previewImageUrl  = "https://$tenant.sharepoint.com/SiteAssets/contoso-logo.jpeg
     }}
     '
 
-    if( -not ($siteScript = Get-PnPSiteScript | Where-Object -Property "Title" -eq "Public Site Approval Workflow") )
+    if( -not ($siteScript = Get-PnPSiteScript | Where-Object -Property "Title" -eq "Send Site Creation Notificiation" ) )
     {
-        Write-Host "Provisioning Site Script: Public Site Approval Workflow"
+        Write-Host "Provisioning Site Script: Send Site Creation Notificiation"
 
         $schema = $template -f $powerAutomateOrLogicAppTriggerUrl
 
         $siteScript = Add-PnPSiteScript `
-                                -Title       "Public Site Approval Workflow" `
-                                -Description "Initiates an approval workflow to convert a site from private to public." `
+                                -Title       "Send Site Creation Notificiation"  `
+                                -Description "Sends the site owners a welcome notice" `
                                 -Content     $schema
     }
 
 
 # create the site designs
 
-if( -not (Get-PnPSiteDesign | Where-Object -Property "Title" -eq "Public Site Approval Workflow" ) )
+if( -not (Get-PnPSiteDesign | Where-Object -Property "Title" -eq "Send Site Creation Notificiation" ) )
 {
-    Write-Host "Provisioning Site Design: Public Site Approval Workflow"
+    Write-Host "Provisioning Site Design: Send Site Creation Notificiation"
 
-    Add-PnPSiteDesign `
-        -Title           "Public Site Approval Workflow" `
-        -Description     "The template initiates an approval workflow for converting a SharePoint Online site from private to public." `
-        -ThumbnailUrl    $previewImageUrl `
-        -SiteScriptIds   $siteScript.Id `
-        -WebTemplate     "TeamSite"
+    $design = Add-PnPSiteDesign `
+                    -Title           "Send Site Creation Notificiation"  `
+                    -Description     "Sends the site owners a welcome notice" `
+                    -ThumbnailUrl    $previewImageUrl `
+                    -SiteScriptIds   $siteScript.Id `
+                    -WebTemplate     "TeamSite" `
+                    -IsDefault
+
+    Grant-PnPSiteDesignRights `
+        -Identity $design.Id `
+        -Principals "joe.rodgers@josrod.onmicrosoft.com", "c:0t.c|tenant|986b904f-0de9-416d-9fd9-7e5d8402e7c0" `
+        -Rights View
 }
-
 
 <# 
 
 # Remove Solution Commands
 
-    Get-PnPSiteDesign | Where-Object -Property "Title" -eq "Public Site Approval Workflow" | Remove-PnPSiteDesign -Force
-    Get-PnPSiteScript | Where-Object -Property "Title" -eq "Public Site Approval Workflow" | Remove-PnPSiteScript -Force
+    Get-PnPSiteDesign | Where-Object -Property "Title" -eq "Send Site Creation Notificiation" | Remove-PnPSiteDesign -Force
+    Get-PnPSiteScript | Where-Object -Property "Title" -eq "Send Site Creation Notificiation" | Remove-PnPSiteScript -Force
 
 #>
